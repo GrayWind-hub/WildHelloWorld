60f58ad4f713cca83453e7d0bce25a60c04edcec
Sergey Golov <sergey.golov@gmail.com>
2020-12-14 14:41:09 +0300

    http test id

--- src/main/java/sgo/wild/HelloService.java
+++ src/main/java/sgo/wild/HelloService.java
@@ -5,22 +5,29 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Date;
 
 
 @Path("/hello")
 public class HelloService {
+    private long id = 0;
+    public HelloService()
+    {
+        id = new Date().getTime();
+    }
+
     @GET
     @Path("/text")
     public String getHello ()
     {
-        return "hello world!";
+        return "hello world! ("+id+")";
     }
     @GET
     @Path("/json")
     @Produces(MediaType.APPLICATION_JSON)
     public SimpleProperty getPropertyJSON ()
     {
-        SimpleProperty p = new SimpleProperty("key","value");
+        SimpleProperty p = new SimpleProperty("id",""+id);
         return p;
     }
     @GET
--- src/main/java/wild/test/HttpTest.java
+++ src/main/java/wild/test/HttpTest.java
@@ -1,2 +1,39 @@
-package wild.test;public class HttpTest {
+package wild.test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpTest {
+    public static void main(String[] args) {
+        String url = "http://127.0.0.1:8080/helloworld/rest/hello/json";
+        HttpTest t = new HttpTest();
+        System.out.println(t.get(url));
+    }
+    private String get(String request) {
+        try {
+            URL url = new URL(request);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setInstanceFollowRedirects(false);
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "text/plain");
+            connection.setRequestProperty("charset", "utf-8");
+            connection.connect();
+
+            try (final BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String inputLine;
+                final StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                return content.toString();
+            } catch (final Exception ex) {
+                ex.printStackTrace();
+                return "";
+            }
+        }catch (Exception e){e.printStackTrace();}
+        return "Error";
+    }
 }
